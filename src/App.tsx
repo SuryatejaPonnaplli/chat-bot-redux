14d37c930236e781db47716525d3d64d37778795
SuryatejaPonnapalli <Suryateja.p@seanergydigital.com>
2025-03-12 12:16:59 +0530

    small changes were added to app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-// import Login from "./pages/Login/Login";
 import Chat from "./pages/Chat/Chat";
 import LoginForm from "./pages/Login/LoginForm";
 const App = () => {
@@ -7,7 +6,6 @@ const App = () => {
     <>
       <BrowserRouter>
         <Routes>
-          {/* <Route path="/" element={<Login />} /> */}
           <Route path="/" element={<LoginForm />} />
           <Route path="/chat" element={<Chat />} />
         </Routes>
